fb84b821ff43458a2d38b0046b51b930d8a5b46d
David Foote <david.a.foote@gmail.com>
2019-06-13 18:31:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def prime?(num)
+  if num == 1 || num == 0 || num < 0
+    return false
+  end
+  (2..num - 1).to_a.all? do |possible_factor| 
+    num % possible_factor != 0
+  end
+end
